3957d709c8300c36f7f43e5a129588c6aca2c36f
// Rohan // <nafisislamrohan@gmail.com>
2025-08-24 19:31:40 +0600

    Updated project with new files

---
+++ examPrac/examPrac/Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using examPrac.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace examPrac.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        regDBMSEntities db = new regDBMSEntities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
