5fd2883c510c4dd82a68a10722c91d23ae0a3804
NafisRohan12 <nafisislamrohan@gmail.com>
2025-08-31 04:08:04 +0600

    Zero hunger

---
+++ ZeroHunger/ZeroHunger/Controllers/RestaurentController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.DTOs;
+using ZeroHunger.EF;
+
+namespace ZeroHunger.Controllers
+{
+    public class RestaurentController : Controller
+    {
+        // GET: Restaurent
+        ZeroHungerDBMSEntities db = new ZeroHungerDBMSEntities();
+
+        public static Mapper GetMapper() {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<CollectReq,CollectReqDTO>().ReverseMap();
+                cfg.CreateMap<Restaurent,RestaurentDTO>().ReverseMap();
+            });
+            return new Mapper(config);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create() { 
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create (CollectReqDTO c)
+        {
+            var data = GetMapper().Map<CollectReq>(c);
+
+            if(data != null)
+            {
+                data.StatusId = 1;
+                data.EmpId = 1;
+                data.RId = 1;
+                db.CollectReqs.Add(data);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ZeroHunger/ZeroHunger/DTOs/CollectReqDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZeroHunger.DTOs
+{
+    public class CollectReqDTO
+    {
+        public int Id { get; set; }
+        public string FoodName { get; set; }
+        public int RId { get; set; }
+        public System.TimeSpan MaxPresTime { get; set; }
+        public int StatusId { get; set; }
+        public int EmpId { get; set; }
+    }
+}
