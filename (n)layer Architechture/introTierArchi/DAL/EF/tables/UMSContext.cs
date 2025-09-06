25e80f0760a5a553edf00bc56fcd7e01f4af4bdf
NafisRohan12 <nafisislamrohan@gmail.com>
2025-09-07 04:15:48 +0600

    incomplete Archi

---
+++ (n)layer Architechture/introTierArchi/DAL/EF/tables/UMSContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.tables
+{
+    public class UMSContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
